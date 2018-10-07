5bde308128b4b198d8d074f24d580149abbcd1ea
Bensoncasey7 <bensoncasey7@gmail.com>
2018-10-06 19:04:11 -0500

    Updates

---
+++ precache-manifest.0025921631b08939fe799e8a353b704a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "de2a5f06d16b7003d1a5",
+    "url": "/portfolio/static/js/main.de2a5f06.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/portfolio/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "de2a5f06d16b7003d1a5",
+    "url": "/portfolio/static/css/main.98b0a36e.chunk.css"
+  },
+  {
+    "revision": "66fec92bd3b7caa43e4c4f9021094192",
+    "url": "/portfolio/index.html"
+  }
+];
