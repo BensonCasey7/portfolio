554f400b050c86d2f2b34c31e70061f0bbf3f08c
Bensoncasey7 <bensoncasey7@gmail.com>
2020-05-20 00:30:04 -0500

    Added basic structure for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,23 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className={'page'}>
-      Navbar
+    <div className={'navbar'}>
+      <div className={'grid-x navbar__container'}>
+        <div className={'cell medium-4 navbar__link'}>
+          LOGO
+        </div>
+        <div className={'cell medium-4 navbar__title'}>
+          CASEY BENSON
+        </div>
+        <div className={'cell medium-4 navbar__link-group'}>
+          <div className={'navbar__link'}>
+            Interactive Resume
+          </div>
+          <div className={'navbar__link'}>
+            Contact Me
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
