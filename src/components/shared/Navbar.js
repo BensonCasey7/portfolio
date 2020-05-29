53c033259556d71986a0e2b1fb0436d5e4d15c73
Bensoncasey7 <bensoncasey7@gmail.com>
2020-05-28 21:19:24 -0500

    Added logo, hover states for navbar, updated colors

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../../images/logo_white.svg';
 
 function Navbar(props) {
   const { showNavbarName } = props;
   const nameClass = showNavbarName ? '' : 'navbar__title--hidden';
+  const currentLocation = useLocation();
+
+  function activeLink(path) {
+    return path === currentLocation.pathname ? 'navbar__link--active' : '';
+  }
 
   return (
     <div className={'navbar'}>
       <div className={'grid-x navbar__container'}>
-        <Link to='./' className={'cell medium-4 navbar__link'}>
-          LOGO
-        </Link>
+        <div className={'cell medium-4'}>
+          <Link to='./' className={'navbar__logo-wrapper'}>
+            <img src={logo} alt={'Logo'} title={'Logo'} className={'navbar__logo'}/>
+          </Link>
+        </div>
         <div className={`cell medium-4 navbar__title ${nameClass}`}>
           CASEY BENSON
         </div>
         <div className={'cell medium-4 navbar__link-group'}>
-          <Link to='./resume' className={'navbar__link'}>
+          <Link to='./resume' className={`navbar__link ${activeLink('/resume')}`}>
             Interactive Resume
           </Link>
-          <Link to='./' className={'navbar__link'}>
+          <Link to='./contact' className={`navbar__link ${activeLink('/contact')}`}>
             Contact Me
           </Link>
         </div>
