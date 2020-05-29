e64b3cf20cc9e0340b3692363d1b921a33ac73eb
Bensoncasey7 <bensoncasey7@gmail.com>
2020-05-29 00:26:28 -0500

    Get logo successfuly working as proper, styleable svg

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import logo from '../../images/logo_white.svg';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../images/logo.svg';
 import NavbarLink from './NavbarLink';
 
 function Navbar(props) {
   const { showNavbarName } = props;
-  const nameClass = showNavbarName ? '' : 'navbar__title--hidden';
-  const currentLocation = useLocation();
-
-  function activeLink(path) {
-    return path === currentLocation.pathname ? 'navbar__link--active' : '';
-  }
+  const showName = showNavbarName ? '' : 'navbar__title--hidden';
 
   return (
     <div className={'navbar'}>
       <div className={'grid-x navbar__container'}>
         <div className={'cell medium-4'}>
           <Link to='./' className={'navbar__logo-wrapper'}>
-            <img src={logo} alt={'Logo'} title={'Logo'} className={'navbar__logo'}/>
+            <div className={'navbar__logo'}>
+              <Logo/>
+            </div>
           </Link>
         </div>
-        <div className={`cell medium-4 navbar__title ${nameClass}`}>
+        <div className={`cell medium-4 navbar__title ${showName}`}>
           CASEY BENSON
         </div>
         <div className={'cell medium-4 navbar__link-group'}>
